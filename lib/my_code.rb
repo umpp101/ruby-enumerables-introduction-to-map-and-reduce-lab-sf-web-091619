279f6b93104430af4a2752fe30fc355f0658050d
Mustafa Muse <umpp101@icloud.com>
2019-09-12 17:14:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,8 +56,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do
-    if source_array[i] === false
-    return false
+   return false if source_array[i] === false
     i += 1
   end
   return true
