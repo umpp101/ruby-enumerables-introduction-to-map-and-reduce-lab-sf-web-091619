177ee157802b82f8a0130370661665552d48d226
Mustafa Muse <umpp101@icloud.com>
2019-09-12 17:02:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,16 +36,25 @@ def map_to_square(source_array)
   i = 0
   new_array = []
   while i < source_array.length do
-    new_array.push(source_array[i] *source_array[i]  )
+    new_array.push(source_array[i] *source_array[i] )
     i += 1
   end
    new_array
 end
 
-
+def educe_to_total(source_array, starting_point = 0)
+  total = starting_point
+  i = 0 
+  new_array = []
+  while i < source_array.length do
+    new_array.push += (source_array[i] + starting_point  )
+    i += 1
+  end
+  total
+end
 # def reduce_to_total(source_array, starting_point = 0)
-#     source_array.reduce(starting_point) { |sum, num| sum + num}
-# end
+# #     source_array.reduce(starting_point) { |sum, num| sum + num}
+# # end
 
 
 
