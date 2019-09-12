8ce3de6f01dcdff19db572ad67793722805d0106
Mustafa Muse <umpp101@icloud.com>
2019-09-12 16:59:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,6 +9,7 @@ def map_to_negativize(source_array)
    new_array
 end
 
+
 def map_to_no_change(source_array)
   i = 0
   new_array = []
@@ -18,6 +19,18 @@ def map_to_no_change(source_array)
   end
    new_array
 end
+
+
+def map_to_double(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    new_array.push(source_array[i] * 2)
+    i += 1
+  end
+   new_array
+end
+
 
 # def map_to_square(source_array)
 #   i =0 
