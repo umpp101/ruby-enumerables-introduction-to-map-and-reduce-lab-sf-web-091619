58ea7587754dd62b1b4b73e0c1c36c1a5299f21c
Mustafa Muse <umpp101@icloud.com>
2019-09-12 17:00:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,15 +32,15 @@ def map_to_double(source_array)
 end
 
 
-# def map_to_square(source_array)
-#   i =0 
-#   new_array =[]
-#   while i< source_array.length do
-#     new_array[i] = source_array[i] * source_array[i]
-#     i =i +1 
-#   end
-#   return new_array
-# end
+def map_to_square(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    new_array.push(source_array[i] *source_array[i]  )
+    i += 1
+  end
+   new_array
+end
 
 
 # def reduce_to_total(source_array, starting_point = 0)
