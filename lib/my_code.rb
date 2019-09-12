f2e4f2cc5bce96449ed14e5cad3fbc34a030b06b
Mustafa Muse <umpp101@icloud.com>
2019-09-12 16:57:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,42 +9,42 @@ def map_to_negativize(source_array)
    new_array
 end
 
-def map_to_no_change(source_array)
- return source_array
-end
+# def map_to_no_change(source_array)
+# return source_array
+# end
 
-def map_to_double(source_array)
-  i =0 
-  new_array =[]
-  while i< source_array.length do
-    new_array[i] = 2 * source_array[i]
-    i =i +1 
-  end
-  return new_array
-end
+# def map_to_double(source_array)
+#   i =0 
+#   new_array =[]
+#   while i< source_array.length do
+#     new_array[i] = 2 * source_array[i]
+#     i =i +1 
+#   end
+#   return new_array
+# end
 
-def map_to_square(source_array)
-  i =0 
-  new_array =[]
-  while i< source_array.length do
-    new_array[i] = source_array[i] * source_array[i]
-    i =i +1 
-  end
-  return new_array
-end
-
-
-def reduce_to_total(source_array, starting_point = 0)
-    source_array.reduce(starting_point) { |sum, num| sum + num}
-end
+# def map_to_square(source_array)
+#   i =0 
+#   new_array =[]
+#   while i< source_array.length do
+#     new_array[i] = source_array[i] * source_array[i]
+#     i =i +1 
+#   end
+#   return new_array
+# end
 
 
+# def reduce_to_total(source_array, starting_point = 0)
+#     source_array.reduce(starting_point) { |sum, num| sum + num}
+# end
 
-def reduce_to_all_true(source_array)
-  source_array.reduce{ |truthy, falsey| truthy && falsey}
-end
 
-def reduce_to_any_true(source_array)
-   source_array.reduce{ |true, source_array| true == true}
-end
+
+# def reduce_to_all_true(source_array)
+#   source_array.reduce{ |truthy, falsey| truthy && falsey}
+# end
+
+# def reduce_to_any_true(source_array)
+#   source_array.reduce{ |true, source_array| true == true}
+# end
 
